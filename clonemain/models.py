b42ad2892a59c9de2854ebd3603e6c6b9a7e3f54
Amwata Albert <albertotieno41@mail.com>
2020-10-22 13:19:46 +0300

    added cloudinary

--- clonemain/models.py
+++ clonemain/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class Post(models.Model):
-    image = models.ImageField(upload_to='images')
+    image = CloudinaryField('image')
     desription = models.CharField(max_length=50)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- insta/settings.py
+++ insta/settings.py
@@ -11,6 +11,7 @@ import os
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
+import cloudinary
 
 
 
@@ -37,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -177,3 +179,9 @@ LOGIN_URL = 'login'
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+
+cloudinary.config( 
+  cloud_name = "dpbnkmqav", 
+  api_key = "458414412259715", 
+  api_secret = "5PMJ6nrmV3NF3HmP5Jg-Gihq4_Q" 
+)
